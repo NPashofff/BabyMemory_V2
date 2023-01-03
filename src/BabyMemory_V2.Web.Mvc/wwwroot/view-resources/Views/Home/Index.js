768c8e45e410c2a310ad053904d9e194809760c9
niki pashofff <niki83@abv.bg>
2023-01-03 19:49:36 +0200

    Stiling newse carts

--- src/BabyMemory_V2.Web.Mvc/wwwroot/view-resources/Views/Home/Index.js
+++ src/BabyMemory_V2.Web.Mvc/wwwroot/view-resources/Views/Home/Index.js
@@ -32,10 +32,10 @@
         <a href="${article.url}" target="_blank">Read more</a>
       `;
                 const articleCard = document.createElement('div');
-                articleCard.className = "card";
+                articleCard.className = "card col-12 col-sm-6 col-md-4 mb-2 d-flex align-items-stretch flex-column";
                 articleCard.style = "width: 18rem;";
                 articleCard.innerHTML = `
-    <img class="card-img-top" src="${article.urlToImage}" alt="Card image cap">
+    <img class="card-img-top " src="${article.urlToImage}" border="true" alt="Card image cap">
     <div class="card-body">
         <h5 class="card-title"><b>${article.title}<b/></h5>
         <p class="card-text"><i>${article.description}<i/></p>
